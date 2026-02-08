12e5a7e4ed50dda6f5a64041bd3ff074851c6e45
Влада Куприенко <kuprienkovladislava@gmail.com>
2026-02-08 15:38:58 +0300

    Добавлены некоторые функции обработки данных

--- defs.py
+++ defs.py
@@ -2,12 +2,12 @@ import pandas as pd
 import sqlite3
 print("defs.py подключен!")
 
-class UnsupportedTableName(Exception): 
-    """
-    Ошибка: неподдерживаемая операция
-    """
-    def __str__(self):
-        return "Ошибка: неподдерживаемая операция"
+# class UnsupportedTableName(Exception): 
+#     """
+#     Ошибка: неподдерживаемая операция
+#     """
+#     def __str__(self):
+#         return "Ошибка: неподдерживаемая операция"
     
 # загрузка данных	load_data
 def load_data(db_link:str, table_name:str):
@@ -26,8 +26,40 @@ def load_data(db_link:str, table_name:str):
     return df_data
 
 # запись данных в файлы	save_data
-def save_data(df, file_name):
-    df.to_excel(f'data/{file_name}.xlsx', index=False)
+def save_data(df, folder, file_name):
+    df.to_excel(f'{folder}/{file_name}.xlsx', index=False)
 
 
+# поиск средней длительности треков avr_duration
+def avr_duration(tracks, genres):
 
+    merged = tracks.merge(genres, on='GenreId')
+    merged_mean = merged.groupby(genres['Name'])['Milliseconds'].mean().reset_index()
+    
+    return merged_mean
+
+# объединение таблиц  combining_tables и выбор необходимой информации
+
+def combining_tables(tracks, albums, artists):
+
+    merge1 = tracks.merge(albums, on='AlbumId')
+    merge2 = merge1.merge(artists, on='ArtistId')
+    results_table = merge2[['Name_x', 'Title', 'Name_y']].rename(columns={'Name_x': 'SongName', 'Name_y': 'ArtistName'}) 
+    return results_table
+
+
+# топ клиентов, по покупке в жанре   purchases_in_genre
+def purchases_in_genre(genres, search_genre, tracks, invoice_items, invoices, customers):
+   
+    genre_row = genres.loc[genres['Name'] == search_genre, 'GenreId']
+    
+    if genre_row.empty: 
+        return pd.DataFrame() # жанр не найден
+    genre_id = genre_row.iloc[0]
+
+    rock_tracks = tracks.loc[tracks['GenreId'] == genre_id, 'TrackId']
+    rock_items = invoice_items.merge(rock_tracks, on='TrackId')
+    rock_invoice = invoices.merge(rock_items, on='InvoiceId')
+    customersid = rock_invoice.groupby('CustomerId').size().reset_index(name='TrackCount')
+    customers_info = customers.merge(customersid, on='CustomerId').sort_values('TrackCount', ascending=False)
+    return customers_info
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Импортирование библиотек 
 import pandas as pd # Для работы с данными в формате таблиц 
-from defs import load_data, save_data 
+from defs import load_data, save_data, avr_duration, combining_tables, purchases_in_genre 
 
 # Список названий таблиц из базы данных
 tables = [
@@ -22,7 +22,22 @@ dfs = {name: load_data('chinook.db', name) for name in tables}
 
 # Сохранение данных в локальное хранилище
 for table_name, table_data in dfs.items():
-    save_data(dfs[table_name], table_name)
+    save_data(dfs[table_name], 'data', table_name)
+
+# Найдите среднюю длительность треков (Milliseconds) в каждом жанре (genres).
+df_avr_duration = avr_duration(dfs['tracks'], dfs['genres'])
+save_data(df_avr_duration, 'results', 'avr_duration')
+
+# Объедините таблицы tracks, albums и artists. 
+# Выведите список треков с названием альбома и именем 
+# исполнителя в xlsx
+df_combining_tables = combining_tables(dfs['tracks'], dfs['albums'], dfs['artists'])
+save_data(df_combining_tables, 'results', 'tracks_album_artist')
 
 
+
+# - Найдите клиентов (customers), которые купили больше всего 
+# треков в жанре "Rock".
+top_genre_customers = purchases_in_genre('Rock', dfs['genres'], dfs['tracks'], dfs['invoice_items'], dfs['invoices'], dfs['customers'])
+save_data(top_genre_customers, 'results', 'top_genre_customers')
 
